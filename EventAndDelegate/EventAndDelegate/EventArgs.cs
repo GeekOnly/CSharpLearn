c836788dbe97392ae4726a749696a5dee7d69d15
GeekOnly <xs13@hotmail.co.th>
2023-09-04 16:28:53 +0700

    add Delegate

---
+++ EventAndDelegate/EventAndDelegate/EventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class ExampleClass
+{
+
+    public delegate void DoSomething(int num);
+
+    public DoSomething Something;
+
+    public ExampleClass()
+    {
+        Something = FunctionOne;
+
+        Something?.Invoke(123);
+
+        Something = FunctionTwo;
+
+        Something?.Invoke(987);
+    }
+
+    private void FunctionTwo(int num)
+    {
+        Console.WriteLine($"Function two called with value: {num}");
+    }
+    private void FunctionOne(int num)
+    {
+        Console.WriteLine($"Function two called with value: {num}");
+    }
+}
